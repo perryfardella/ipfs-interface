4a89ff715f60ffc923e16b40e682c5426286dce1
Perry Fardella <perry@hatchd.com.au>
2022-04-26 10:28:16 +0800

    Begin work within app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+
+const App = () => {
+  const [file, setFile] = useState(null);
+  const retrieveFile = (e: any) => {
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      console.log("Buffer data: ", Buffer(reader.result));
+    }
+  
+    e.preventDefault();  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="file" name="data" onChange={retrieveFile} />
+        <button type="submit" className="btn">Upload file</button>
+      </form>
     </div>
   );
 }
